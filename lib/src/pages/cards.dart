b08b6c8bdec7cd380ca7710fcf1a7d77d1743079
seohyj <linajoo03@gmail.com>
2024-06-28 20:00:46 +0900

    Feat: Cards tab first commit

--- lib/src/pages/cards.dart
+++ lib/src/pages/cards.dart
@@ -1,3 +1,6 @@
+// images of business card using grid view
+// business card ratio: 9:5
+
 import 'package:flutter/material.dart';
 
 class Cards extends StatefulWidget {
@@ -8,8 +11,56 @@ class Cards extends StatefulWidget {
 }
 
 class _CardsState extends State<Cards> {
+
+  List<String> imagesList = [
+    'assets/images/1.png',
+    'assets/images/2.png',
+    'assets/images/3.png',
+    'assets/images/4.png',
+    'assets/images/5.png',
+    'assets/images/6.png',
+    'assets/images/7.png',
+    'assets/images/8.png',
+    'assets/images/9.png',
+    'assets/images/10.png',
+    'assets/images/11.png',
+    'assets/images/12.png',
+    'assets/images/13.png',
+    'assets/images/14.png',
+    'assets/images/15.png',
+    'assets/images/16.png',
+    'assets/images/17.png',
+    'assets/images/18.png',
+    'assets/images/19.png',
+    'assets/images/20.png' ,   
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: GridView.builder(
+        shrinkWrap: true,
+        physics: ScrollPhysics(),
+        scrollDirection:Axis.vertical,
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 1,
+          childAspectRatio: 9.0 / 5.0,
+          mainAxisSpacing: 10.0,
+          crossAxisSpacing: 10.0,
+        ),
+        itemCount: imagesList.length,
+        itemBuilder: (BuildContext context, int index){
+          return Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(5.0),
+              image: DecorationImage(
+                image: AssetImage(imagesList[index]),
+                fit: BoxFit.cover,
+              ),
+            ),
+          );
+        },
+      ),
+    );
   }
 }
